257f6dbd19f103ed75a0c13e5fc237c31ffdf9a7
Ruy Valle <ruyvallem@gmail.com>
2019-12-30 19:26:50 -0500

    Solve day 4 part 2
    Refactors part 1 slightly to share parts of the solution with part 2.

--- day4.rkt
+++ day4.rkt
@@ -2,12 +2,26 @@
 
 (define-values (MIN-VAL MAX-VAL) (values 108457 562041))
 
-(define (solve MIN-VAL MAX-VAL)
+(define (solve min-val max-val valid-solution?)
   (for/fold ([n-solutions 0])
-            ([val (in-range MIN-VAL (+ MAX-VAL 1))])
+            ([val (in-range min-val (+ max-val 1))])
     (if (valid-solution? (number->digit-list val))
         (+ n-solutions 1)
         n-solutions)))
+
+(define (part1)
+  (solve MIN-VAL
+         MAX-VAL
+         (lambda (num/list)
+           (and (has-repeated-digits? num/list)
+                (ltr-digits-nondecreasing? num/list)))))
+
+(define (part2)
+  (solve MIN-VAL
+         MAX-VAL
+         (lambda (num/list)
+           (and (has-1+pair-repeated-digits? num/list)
+                (ltr-digits-nondecreasing? num/list)))))
 
 (define (number->digit-list num)
   (foldr
@@ -17,16 +31,25 @@
          (cons (string->number digit) digit-list)))
    '()
    (string-split (format "~a" num) "")))
-
-(define (valid-solution? num/list)
-  (and (has-repeated-digits? num/list)
-       (ltr-digits-nondecreasing? num/list)))
   
 (define (has-repeated-digits? num/list)
   (cond
     [(= (length num/list) 1) #f]
     [(= (first num/list) (second num/list)) #t]
     [else (has-repeated-digits? (rest num/list))]))
+
+(define (has-1+pair-repeated-digits? num/list)
+  (cond
+    [(<= (length num/list) 1) #f]
+    [(and (= (length num/list) 2) (= (first num/list) (second num/list)))
+     #t]
+    [(= (length num/list) 2) #f]
+    [(= (first num/list) (second num/list) (third num/list))
+     (has-1+pair-repeated-digits?
+      (dropf num/list
+             (lambda (num) (= num (first num/list)))))]
+    [(= (first num/list) (second num/list)) #t]
+    [else (has-1+pair-repeated-digits? (rest num/list))]))
 
 (define (ltr-digits-nondecreasing? num/list)
   (cond
@@ -36,4 +59,5 @@
     [else #f]))
 
 (module+ main
-  (displayln (solve MIN-VAL MAX-VAL)))
+  (displayln (part1))
+  (displayln (part2)))
